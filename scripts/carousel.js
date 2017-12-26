81da360d6b87cadae169bb11052a479cea7ab844
madigan-99 <fymadigan@outlook.com>
2017-12-25 20:20:41 -0800

    promotions update

--- scripts/carousel.js
+++ scripts/carousel.js
@@ -4,7 +4,7 @@ showSlides();
 function showSlides() {
     var i;
     var slides = document.getElementsByClassName("mySlides");
-    var dots = document.getElementsByClassName("dot");
+    var counter = document.getElementsByClassName("counter");
 
     for (i = 0; i < slides.length; i++) {
        slides[i].style.display = "none";  
@@ -14,11 +14,11 @@ function showSlides() {
 
     if (slideIndex > slides.length) {slideIndex = 1}    
 
-    for (i = 0; i < dots.length; i++) {
-        dots[i].className = dots[i].className.replace(" active", "");
+    for (i = 0; i < counter.length; i++) {
+        counter[i].className = counter[i].className.replace(" active", "");
     }
-    
+
     slides[slideIndex-1].style.display = "block";  
-    dots[slideIndex-1].className += " active";
+    counter[slideIndex-1].className += " active";
     setTimeout(showSlides, 3000); // Change image every 2 seconds
 }
